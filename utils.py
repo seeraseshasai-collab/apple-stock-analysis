7095d3b556e4e9e05a3859673d2dbdcb1fbc3d38
seshasai <seera.seshasai@gmail.com>
2026-03-19 21:34:02 +0530

    test

--- utils.py
+++ utils.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from capstone!")
+import pandas as pd
 
 
-if __name__ == "__main__":
-    main()
+
+
+# function to import data from csv file
+def import_data(file_path):
+    try:
+        data = pd.read_csv(file_path)
+        return data
+    except Exception as e:
+        print(f"Error importing data: {e}")
+        return None
